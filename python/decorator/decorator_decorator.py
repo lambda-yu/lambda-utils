1d2c98f4d9df1ecbab85de415e55a195fb75be6b
lambda <yuliguuang@outlook.com>
2023-03-13 22:19:28 +0800

    add same annotation

--- python/decorator/decorator_decorator.py
+++ python/decorator/decorator_decorator.py
@@ -42,6 +42,7 @@ def wrap(decorator_func: Callable):
 
 
 # 我想写一个装饰器，但是我比较懒，于是用一个装饰器把函数装饰为装饰器，达到了写一个装饰器一样的效果~！
+# 这是一个不带参装饰器
 @wrap
 def dog_bark(func):
     print("eat before wang~!")
@@ -50,6 +51,7 @@ def dog_bark(func):
     return result
 
 
+# 这是一个带参装饰器
 @wrap_parameter
 def dog_bark_p(func, a, b):
     print(f"eat before {'wang~! ' * a}")
@@ -58,11 +60,13 @@ def dog_bark_p(func, a, b):
     return result
 
 
+# 这是使用一个不带参装饰器
 @dog_bark
 def dog_eat(a):
     [print(f"dog eat hot dog {i}~!") for i in range(a)]
 
 
+# 这是使用一个带参装饰器
 @dog_bark_p(b=5, a=3)
 def dog_eat_p(a):
     [print(f"dog eat hot dog {i}~!") for i in range(a)]
