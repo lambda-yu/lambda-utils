1b6872524f3c3d8a7f44197b9ca77bfb386ccecd
lambda <yuliguuang@outlook.com>
2023-03-13 22:14:36 +0800

    add python decorator_decorator.py

---
+++ python/decorator/decorator_decorator.py
@@ -0,0 +1,73 @@
+from typing import Callable
+from functools import wraps
+
+"""
+将一个 函数 装饰为 装饰器 的装饰器~！
+"""
+
+
+def wrap_parameter(decorator_func: Callable):
+    @wraps(decorator_func)
+    def parameter(*de_args, **de_kwargs):
+        def inner(call_func: Callable):
+            @wraps(call_func)
+            def i_inner(*args_i, **kwargs_i):
+                def call():
+                    return call_func(*args_i, **kwargs_i)
+
+                decorator_func(call, *de_args, **de_kwargs)
+                return decorator_func
+
+            return i_inner
+
+        return inner
+
+    return parameter
+
+
+def wrap(decorator_func: Callable):
+    @wraps(decorator_func)
+    def inner(call_func: Callable):
+        @wraps(call_func)
+        def i_inner(*args_i, **kwargs_i):
+            def call():
+                return call_func(*args_i, **kwargs_i)
+
+            decorator_func(call)
+            return decorator_func
+
+        return i_inner
+
+    return inner
+
+
+# 我想写一个装饰器，但是我比较懒，于是用一个装饰器把函数装饰为装饰器，达到了写一个装饰器一样的效果~！
+@wrap
+def dog_bark(func):
+    print("eat before wang~!")
+    result = func()
+    print("eat after wang~!")
+    return result
+
+
+@wrap_parameter
+def dog_bark_p(func, a, b):
+    print(f"eat before {'wang~! ' * a}")
+    result = func()
+    print(f"eat before {'wang~! ' * b}")
+    return result
+
+
+@dog_bark
+def dog_eat(a):
+    [print(f"dog eat hot dog {i}~!") for i in range(a)]
+
+
+@dog_bark_p(b=5, a=3)
+def dog_eat_p(a):
+    [print(f"dog eat hot dog {i}~!") for i in range(a)]
+
+
+if __name__ == '__main__':
+    dog_eat(2)
+    dog_eat_p(3)
